c6f952187218a53b374b3accb1d52936dd6bf1cb
Hpalling <105853474+Hpalling@users.noreply.github.com>
2022-06-01 18:38:28 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace StrindsAndArrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //prog küsib eesnime
+            //prog kuvab nime pikkuse
+
+            Console.WriteLine("sisesta eesnimi");
+            string firstName = Console.ReadLine();
+
+            //int firstNameLenght = firstName.Lenght;
+            Console.WriteLine($"Sinu eesnimes on {firstName.Length} sümbolit.");
+        }
+    }
+}
+
